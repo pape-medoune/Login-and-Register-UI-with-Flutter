40cb52dd5fdb21b0bc8bc65edd9465f6ab069c5f
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-09-23 13:50:19 +0000

    Mise à jours

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,9 +9,6 @@ class Login extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(
-              height: 60,
-            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
@@ -100,25 +97,101 @@ class Login extends StatelessWidget {
                     ],
                   ),
                   const SizedBox(
-                    height: 24,
+                    height: 14,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Not a member?",
+                        style: TextStyle(
+                          fontSize: 19,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      InkWell(
+                        child: const Text(
+                          "Sign in",
+                          style: TextStyle(
+                            fontSize: 19,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onTap: () {},
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 3),
+                        width: 98,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                      const Text(
+                        "OR CONTINUE ",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 3),
+                        width: 98,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 25,
                   ),
                   Column(
                     children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 128,
-                            vertical: 10,
+                      Container(
+                        padding: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                          // border: Border.all(
+                          //   width: 0.5,
+                          //   color: Colors.black,
+                          // ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(15),
                           ),
-                          backgroundColor: Colors.green,
-                          alignment: Alignment.center,
+                          color: Colors.grey[300],
                         ),
-                        child: const Text("Register",
-                            style: TextStyle(
-                              fontSize: 25,
-                            )),
-                      )
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/images/Google_ G _Logo.svg.png',
+                                width: 50,
+                                height: 50,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const Text(
+                              "Continue with Google",
+                              style: TextStyle(
+                                  fontSize: 23, fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ],
